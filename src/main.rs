633e2d6a3cea1485598df94e663ed944e9e56d28
Antoine Reversat <a.reversat@gmail.com>
2024-12-16 21:38:26 -0500

    Rework to only use usb device and AES encryption

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,38 @@
 use clap::Parser;
-use anyhow::{Context, Result, bail};
+use anyhow::{Result, bail};
+use aes_gcm::{Aes256Gcm, Key, KeyInit, Nonce};
+use aes_gcm::aead::Aead;
 
 /// Unlock Some Binary
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None, author)]
 struct Args {
-    /// The unlock password
-    #[arg(short, long)]
-    password: String,
 }
 
-fn try_unlock(pw: u32, vidpid: u32) -> bool {
-    pw ^ vidpid == 0x1e528ddc
+const ENCRYPTED: [u8; 46] = [0x77,0x59,0xf5,0x4,0xd5,0x61,0xd1,0xe7,0x84,0x51,0xb2,0xec,0xf5,0x1a,0x27,0x2c,0x1,0x7d,0x89,0xa7,0xad,0xa5,0xc6,0x58,0x9f,0xf3,0xf5,0x8a,0xc9,0x43,0x27,0x1,0x7a,0x13,0xb,0xc9,0xe5,0x2,0xc0,0x6,0x29,0x81,0xa7,0x42,0x43,0xe9];
+
+fn try_decrypt(vidpid: u32) -> Option<Vec<u8>> {
+    let nonce = Nonce::default();
+    let key_bytes: Vec<u8> = (0..8).into_iter().map(|_| vidpid.to_be_bytes()).flatten().collect();
+    let key: &Key<Aes256Gcm> = Key::<Aes256Gcm>::from_slice(&key_bytes);
+    let cipher = Aes256Gcm::new(key);
+
+    cipher.decrypt(&nonce, &ENCRYPTED[..]).ok()
 }
 
 fn main() -> Result<()>{
     let args = Args::parse();
-    let password = args.password.parse::<u32>().context("Invalid password")?;
 
     for device in rusb::devices()?.iter() {
         let device_desc = device.device_descriptor()?;
+        if device_desc.vendor_id() != 0x8086 {
+            continue;
+        }
         let vidpid = ((device_desc.vendor_id() as u32) << 16) | device_desc.product_id() as u32;
-        if try_unlock(password, vidpid) {
-            println!("flag{{{:8x}_and_coffee}}", 0xc0ff3333^password^vidpid);
+        if let Some(cleartext) = try_decrypt(vidpid) {
+            println!("{}", String::from_utf8(cleartext)?);
             return Ok(());
         }
     }
-    bail!("Nope.");
+    bail!("Missing beefy intel key.");
 }
